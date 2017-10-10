bd7a6febe9c6a58eaebc741cbfde6c7a485e3965
João Paulo Hildebrand Landino <joao.landino@DMCONTROLE>
2017-10-10 15:57:02 -0300

    Componentes e serviços gerados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
+
+import { GeocodeService } from './services/geocode.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputComponent
+    InputComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    GeocodeService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
